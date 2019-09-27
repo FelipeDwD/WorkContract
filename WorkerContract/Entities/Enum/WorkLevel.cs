1bd52e0c97861019d6303921445f3061ff833c98
Felipe Neves de Arruda <felipeneves089@gmail.com>
2019-09-27 10:52:53 -0300

    first commit

---
+++ WorkerContract/Entities/Enum/WorkLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerContract.Entities.Enum
+{
+    enum WorkLevel
+    {
+        JUNIOR,
+        MID_LEVEL,
+        SENIOR
+    }
+}
---
+++ WorkerContract/Entities/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkerContract.Entities.Enum;
+
+namespace WorkerContract.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Worker(string name, WorkLevel level, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+
+        }
+
+        public double Income(int year, int month)
+        {
+            return 0;
+        }
+    }
+}
